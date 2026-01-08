e6f5ca7e03f6ee2129bae877a4b80bf2e6a2c6e0
Mehmet Imga <imgamehmet@gmail.com>
2026-01-07 19:36:50 -0800

    first commit

---
+++ src/mehmetmga_py_524/__init__.py
@@ -0,0 +1,7 @@
+"""Practice package"""
+
+from .core import add_two
+
+__all__ = ["add_two"]
+__version__ = "0.1.0"
+
---
+++ src/mehmetmga_py_524/core.py
@@ -0,0 +1,11 @@
+"""Core helpers for mehmetmga-py-524."""
+
+from typing import Union
+
+Number = Union[int, float]
+
+
+def add_two(x: Number, y: Number) -> Number:
+    """Return the sum of x and y."""
+    return x + y
+
